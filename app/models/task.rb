3dfcb8dc69f906513d91c345befc66de99fa395b
Pineapple <pineappleman@skiff.com>
2023-02-09 01:02:43 -0300

    feat: ✅ tasks scope

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
+# Moder for user tasks
 class Task < ApplicationRecord
   belongs_to :category, optional: true
+
+  scope :complete, -> { where(completed: true) }
+  scope :incoplete, -> { where(completed: false) }
+  scope :sorted, -> { order(:position) }
+  scope :search, ->(kw) { where('LOWER(name) LIKE ?', "%#{kw.downcase}%") }
 end
