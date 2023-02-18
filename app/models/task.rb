698d2ad5eecd41fe281e9d35d810d0ac3a999c77
Pineapple <pineappleman@skiff.com>
2023-02-17 23:46:33 -0300

    chore: 🛠️ created callbacks

--- app/models/task.rb
+++ app/models/task.rb
@@ -11,6 +11,12 @@ class Task < ApplicationRecord
 
   validate :description_has_no_prohibited_words
 
+  before_validation :titleize_name, :set_default_position
+  before_create :log_create
+  before_update :log_update
+  after_save :log_save
+  after_commit :cleaning_reminder
+
   scope :complete, -> { where(completed: true) }
   scope :incoplete, -> { where(completed: false) }
   scope :sorted, -> { order(:position) }
@@ -18,14 +24,41 @@ class Task < ApplicationRecord
 
   private
 
-  def description_has_no_prohibited_words
-    return unless description.present?
+    def description_has_no_prohibited_words
+      return unless description.present?
 
-    prohibited_words = ['later', 'eventually', 'someday']
-    prohibited_words.each do |word|
-      if description.include?(word)
-        errors.add(:description, 'cannot contain prohibited word: ${word}')
+      prohibited_words = ['later', 'eventually', 'someday']
+      prohibited_words.each do |word|
+        if description.include?(word)
+          errors.add(:description, 'cannot contain prohibited word: ${word}')
+        end
       end
     end
-  end
+
+    def titleize_name
+      self.name = name.titleize
+    end
+
+    def set_default_position
+      if position.blank? || position < 1
+        max = Task.maximum(:position) || 0
+        self.position = max + 1
+      end
+    end
+
+    def log_create
+      logger.debug("Task being created: #{name}")
+    end
+
+    def log_update
+      logger.debug("Task being updated: #{name}")
+    end
+
+    def log_save
+      logger.debug("Taskd was saved: #{name}")
+    end
+
+    def cleaning_reminder
+      logger.debug("Remember to prune old tasks")
+    end
 end
